239717842ab36fd544ac010f5774d30ff32245c8
Lakshmi Narayana <lakshminarayana.sekhar@sysmedac.com>
2022-10-24 18:39:16 +0530

    changes stack navigation

--- App.js
+++ App.js
@@ -1,53 +1,64 @@
-import { useEffect, useState } from "react";
-import { View, Text, Image } from "react-native";
-import AppPicker from "./app/components/AppPicker";
-import AppText from "./app/components/AppText";
-import AppTextInput from "./app/components/AppTextInput";
-import Card from "./app/components/Card";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
+import React from "react";
+import { Button, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import Screen from "./app/components/Screen";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import LoginScreen from "./app/screens/LoginScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
-import AppButton from "./app/components/AppButton";
+const Links = () => {
+  const navigation = useNavigation();
+  return (
+    <Button
+      title='Click'
+      onPress={() => navigation.navigate("TweetDetails", { id: 2 })}
+    />
+  );
+};
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweet</Text>
+    <Button
+      title='View Tweet'
+      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+    />
+    <Links />
+  </Screen>
+);
+
+const TweetDetails = ({ route }) => (
+  <Screen>
+    <Text>Tweet Details {route.params.id} </Text>
+  </Screen>
+);
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator
+    initialRouteName='Tweets'
+    screenOptions={{
+      headerStyle: { backgroundColor: "tomato" },
+      headerTintColor: "white",
+      headerTitleAlign: 'center'
+    }}
+  >
+    <Stack.Screen
+      name='Tweets'
+      component={Tweets}
+      options={{
+        headerShown: false,
+      }}
+    ></Stack.Screen>
+    <Stack.Screen
+      name='TweetDetails'
+      component={TweetDetails}
+      options={({ route }) => ({ title: route.params.id })}
+    ></Stack.Screen>
+  </Stack.Navigator>
+);
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
-  const requestPermission = async () => {
-    // const result = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.CONTACTS);
-
-    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
-    if (!granted) {
-      alert("You need to enable permission to access the library. ");
-    }
-  };
-  useEffect(() => {
-    requestPermission();
-  }, []);
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled) {
-        console.log(result)
-        setImageUri(result.uri);
-      }
-    } catch (error) {
-      console.log("Error reading an Image", error);
-    }
-  };
   return (
-    <Screen>
-      <AppButton title='select Image' onPress={selectImage} />
-      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-    </Screen>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
